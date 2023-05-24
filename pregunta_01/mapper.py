bcd5d19d8c9ba3cd22ea856950e16128b4ef7a5c
OscarLeoSanchez <olsanchezco@unal.edu.co>
2023-05-24 11:56:23 -0500

    resuelta pregunta 1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,6 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-# import mapper
-print("Aquí estuve yo")
-print("solo yo")
+
+# abrir el archivo de entrada
+# with open('credit.csv', 'r') as f:
+#     for line in f.readlines():
+#         line = line.strip()
+#         line = line.strip()
+#         line = line.split(',')
+#         print(f'{line[2].strip()}\t1')
+
+import sys
+
+for line in sys.stdin:
+    line = line.strip()
+    line = line.strip()
+    line = line.split(',')
+    print(f'{line[2].strip()}\t1')
--- pregunta_01/reducer.py
+++ pregunta_01/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+current_key = None
+current_count = 0
+# with open('file.txt', 'r', encoding='utf-16') as f:
+    # line = f.readlines()
+for line in sys.stdin:
+    line = line.rstrip()
+    line = line.strip()
+    key, value = line.split('\t')
+    value = int(value)
+    if key == current_key:
+        current_count += value
+
+    else:
+        if current_key is not None:
+            print(current_key, current_count)
+        current_key = key
+        current_count = value
+if current_key is not None:
+    print(current_key, current_count)
+
+# cat credit.csv | python3 mapper.py | python3 reducer.py
